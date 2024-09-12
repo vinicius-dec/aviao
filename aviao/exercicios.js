028b5596d4a5359a0afa19d2a5869e1903edd82c
vinicius-dec <viniciusbellonidesouza@gmail.com>
2024-09-12 12:29:31 -0300

    Add files via upload

---
+++ aviao/exercicios.js
@@ -0,0 +1,209 @@
+
+
+
+
+let numero_de_passageiros_adultos= 3
+let numero_de_passageiros_criancas= 2
+let numero_total_de_passageiros= 5
+
+
+const classe= "executivo"
+const destino= "Continente asiatico"
+const norte= "Norte do brasil"
+const sul= "Sul do brasil"
+const asiatico= "Continente asiatico"
+const europeu= "Continente europeu"
+const africano= "Continente africano"
+
+let norte_economica_adulto= 2500
+let norte_economica_crianca= 1800
+
+let norte_executiva_adulto= norte_economica_adulto + (norte_economica_adulto*20 )/100
+let norte_executiva_crianca= norte_economica_crianca + (norte_economica_crianca*10 )/100
+
+let sul_economica= 3000
+let sul_executivo= 4000  
+
+let asiatico_economica= 4600
+let asiatico_executiva= 6000
+
+let europeu_economica= 5800
+let europeu_executivo= 7300
+
+let africano_economica= 4200
+let africano_executivo= 6900
+
+let custo_total_executivo= 7
+let custo_total_economico= 7
+
+if (destino=norte){
+    if (classe="executivo"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+
+    }
+    else if(classe="economico"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+  
+}
+
+}
+else if(destino=sul){
+    if(classe="executivo"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+        
+    }
+    else if(classe="economico"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+
+    }
+    
+}
+else if(destino=asiatico){
+    if(classe="executivo"){
+        console.log("*** O custo total do seu pacote é: ***")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+
+    }
+    else if(classe="economico"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+
+    }
+}
+else if(destino=africano){
+    if(classe="executivo"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+
+    }
+    else if(classe="economica"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+
+    }
+
+}
+else if(destino=europeu){
+    if(classe="executivo"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+    }
+    else if(classe="economica"){
+        console.log("*** O custo total do seu pacote é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Adultos é:")
+        console.log()
+        console.log("A Quantidade de Passageiros Infantis é:")
+        console.log()
+        console.log("O destino escolhido é:")
+        console.log()
+        console.log("A Classe do Seu Voo é:")
+        console.log()
+        console.log("O custo total da viagem é:")
+        console.log()
+        console.log("*** Fim do programa ***")
+    }
+}
